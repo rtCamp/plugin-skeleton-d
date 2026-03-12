bedde7caa3723ba8ff64a8e50298f4dfcadf4f59
Dovid Levine <david@axepress.dev>
2026-03-12 02:59:35 +0200

    deps: Update dependencies and workflows (#8)
    * deps: Update dependencies and workflows
    
    * chore: cleanup configs
    
    * chore: add GitHub issue templates

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,11 @@
 import { defineConfig, type PlaywrightTestConfig } from '@playwright/test';
 import path from 'path';
 
-const artifactsPath = path.join( process.cwd(), 'tests/_output' );
+const artifactsPath = path.join( process.cwd(), 'tests/_output/e2e' );
 
-// Ensure WP artifacts (and storage-state) are written into tests/_output
+// Ensure WP artifacts (and storage-state) are written into tests/_output/e2e
 process.env[ 'WP_ARTIFACTS_PATH' ] = artifactsPath;
-// Ensure STORAGE_STATE_PATH points into tests/_output as well
+// Ensure STORAGE_STATE_PATH points into tests/_output/e2e as well
 process.env[ 'STORAGE_STATE_PATH' ] = path.join(
 	artifactsPath,
 	'storage-states',
@@ -21,7 +21,7 @@ const baseConfig =
 const config = defineConfig( {
 	...baseConfig,
 	testDir: './tests/e2e',
-	outputDir: './tests/_output',
+	outputDir: './tests/_output/e2e',
 } );
 
 export default config;
