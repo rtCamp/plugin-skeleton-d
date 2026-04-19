4b1d834c568f25a22ea903ea187b26691c79c20d
Dovid Levine <david@axepress.dev>
2026-04-19 23:04:20 +0300

    fix: reflection

--- tests/php/TestCase.php
+++ tests/php/TestCase.php
@@ -23,7 +23,7 @@ abstract class TestCase extends WP_UnitTestCase {
 	 * @return array<string, array<string, list<string>>>
 	 */
 	public function getAnnotations(): array { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.MethodNameInvalid -- Required compatibility method name.
-		$class_reflection  = new ReflectionClass( static::class );
+		$class_reflection  = new \ReflectionClass( static::class );
 		$method_name       = $this->name();
 		$method_reflection = $class_reflection->hasMethod( $method_name )
 			? $class_reflection->getMethod( $method_name )
